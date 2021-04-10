93e39bfc67c5c255659d41ee64255ed499d01c92
Vincent T <vyncode@gmail.com>
2021-04-10 13:08:32 -0400

    notes outline, need prior functions

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,16 @@
+
+// part 6
+
+function fetchResults(x) {
+    //gets the values from search
+    usersearch = document.querySelector('search')
+    userSearchData = usersearch.value
+    //formats input into url
+    
+    //creates url from getRequestURL()
+
+    //fetch the results?
+
+    //exe displayResults(resA,resB,resC)
+    
+}
