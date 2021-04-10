5bb05dd8f34a4fdad1f8f7a6ec38c1aef0b89b25
Vincent T <vyncode@gmail.com>
2021-04-10 13:51:56 -0400

    added more to func

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,16 +1,39 @@
 
+
+
+/* ONE person will create a function getRequestURL(search,format), 
+this function will return a string, which will be the API URL to be
+fetched for those values. After finishing, add, commit, pull ,
+push and pull request.
+*/
+function getRequestUrl (searchTerm) { 
+    return "https://www.libraryofcongress.gov/search/?"+ "q=" + searchTerm + "&fo=json"
+ }
+
 // part 6
 
 function fetchResults(x) {
     //gets the values from search
-    usersearch = document.querySelector('search')
-    userSearchData = usersearch.value
+    let fetchUsersearch = document.querySelector('search')
+    let fetchUserSearchData = usersearch.value
     //formats input into url
     
     //creates url from getRequestURL()
+    let fetchReq = getRequestUrl(searchTerm)
 
     //fetch the results?
-
+    fetch(fetchReq)
+    .then(response => response.json())
+    .then(data => {
+        let jsonstring = JSON.stringify(data)
+    })
+    .catch( fetchError => {
+        let errorname = fetchError.name
+        let errormessage = fetchError.error
+        alert(`error: ${errorname} ${errormessage}`)
+    })
     //exe displayResults(resA,resB,resC)
     
 }
+// part 6
+
