f217a78b85b1529e8b01b7695186e5556ea15c59
Nate Jones <15nate.jones@gmail.com>
2021-04-10 13:27:03 -0400

    Updating variable id's for html/js connectivity

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,3 +1,10 @@
+var linkBtnDisplay = document.getElementById("readMoreButton");
+
+var textDisplay = document.getElementById("description");
+
+var dateDisplay = document.getElementById("date");
+
+var titleDisplay = document.getElementById("firstSearchResult");
 
 
 function displayResult(title, text, link) {
